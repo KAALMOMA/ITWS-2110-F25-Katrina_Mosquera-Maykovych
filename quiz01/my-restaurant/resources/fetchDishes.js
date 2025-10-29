1ef2a94b3e842336612f391d6042cd691c420fb0
Katrina Mosquera-Maykovych <katrinamosquera@gmail.com>
2025-10-28 23:37:40 -0400

    100% Finished quiz01.

--- quiz01/my-restaurant/resources/fetchDishes.js
+++ quiz01/my-restaurant/resources/fetchDishes.js
@@ -1,7 +1,7 @@
 let data;
 async function fetchDishes() {
     try {
-        const response = await fetch("/quiz01/my-restaurant/data/dishes.json")
+        const response = await fetch("data/dishes.json")
         if(!response.ok) {
             throw new Error("Could not fetch resource!");
         }
--- quiz01/my-restaurant/resources/parseDishes.js
+++ quiz01/my-restaurant/resources/parseDishes.js
@@ -3,7 +3,7 @@ document.addEventListener("DishesReady", (event) => {
     for (let i = 0; i < data.dishes.length; ++i)
     {   
         const imgPath = data.dishes[i].image
-        const name = "<td><span id=\"emphasis\"><b>" + data.dishes[i].name + "</b></span><br><img id=\"dishImage\" src=\"" + imgPath + "\" height=200px alt=\"picture of " + data.dishes[i].name + "\"></img>" + "</td>"
+        const name = "<td><span class=\"emphasis\"><b>" + data.dishes[i].name + "</b></span><br><img id=\"dishImage\" src=\"" + imgPath + "\" height=200px alt=\"picture of " + data.dishes[i].name + "\"></img>" + "</td>"
         const desc = "<td>" + data.dishes[i].description + "</td>"
         const cate = "<td>" + data.dishes[i].category + "</td>"
         const cuis = "<td>" + data.dishes[i].cuisine + "</td>"
